ae2335cfaa60b42f6564ec1f3014a6936af3bf7c
Astrid Mata <86080960+AstridMata@users.noreply.github.com>
2025-11-29 02:41:06 +0000

    models

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Planets
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,24 +28,65 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+# /////////////////////////////////////////////////////////////////////////
 
+# trae todos los usuarios 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    try:
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+        query_results= User.query.all()
+        if not query_results:
+            return jsonify({"msg": "Usuarios no encontrados"}), 400
+        
+        results= list(map(lambda item: item.serialize(), query_results))
 
-    return jsonify(response_body), 200
 
+        response_body = {
+        "msg": "Todo salio bien",
+        "results": results
+        }
+
+        return jsonify(response_body), 200
+    
+    except Exception as e:
+        print(f"Error al obtener usuarios: {e}")
+        return jsonify({"msg": "Internal Server Error", "error": str(e)}), 500
+    
+#//////////////////////////////////////////////////////////////////////// trae un usuario por id  
+@app.route('/user/<int:user_id>', methods=['GET'])
+def user_by_id(user_id):
+    try:
+
+        query_results= User.query.filter_by(id=user_id).first()
+        if not query_results:
+            return jsonify({"msg": "Usuario no existe"}), 400
+        
+        
+        response_body = {
+        "msg": "usuario encontrado",
+        "results": query_results.serialize()
+        }
+
+        return jsonify(response_body), 200
+    
+    except Exception as e:
+        print(f"Error al obtener usuarios: {e}")
+        return jsonify({"msg": "Internal Server Error", "error": str(e)}), 500
+
+# /////////////////////////////////////////////////////////////////////////////////
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    password: Mapped[str] = mapped_column(String(120), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
@@ -16,4 +16,20 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    population: Mapped[str] = mapped_column(String(120), nullable=False)
+    climate: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "climate": self.climate
+            
         }
